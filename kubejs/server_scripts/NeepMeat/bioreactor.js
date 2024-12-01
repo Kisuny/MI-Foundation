e73448144d8cd0110d5399690b2b55ae9733e9fb
Sipepper <60097471+Sipepper@users.noreply.github.com>
2024-12-01 22:56:18 +0200

    Bioreactor recipe, still WIP

---
+++ kubejs/server_scripts/NeepMeat/bioreactor.js
@@ -0,0 +1,30 @@
+
+// ServerEvents.recipes(event => {
+//     event.recipes.neepmeat.bioreactor({
+//         'inputs':
+//             [
+//                 {
+//                     'type': 'minecraft:fluid',
+//                     'resource': 'neepmeat:blood',
+//                     'amount': 1000
+//                 },
+//                 {
+//                     'type': 'minecraft:fluid',
+//                     'resource': 'modern_industrialization:magenta_ink',
+//                     'amount': 1000
+//                 },
+//                 {
+//                     'type': 'minecraft:fluid',
+//                     'resource': 'neepmeat:work_fluid',
+//                     'amount': 1000
+//                 }
+//             ],
+//         'output':
+//         {
+//             'type': 'minecraft:fluid',
+//             'resource': 'modern_industrialization:magenta_ink',
+//             'amount': 3000
+//         },
+//         'process_time': 5
+//     });
+// })
