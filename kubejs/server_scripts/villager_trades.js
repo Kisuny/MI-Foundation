9eaf74c0526caef6d4ed62f1f60460e37ea2d88b
Kisuny <homeca19@gmail.com>
2024-12-02 01:34:31 +0300

    Returned trades for the ae2:fluix_researcher villager

--- kubejs/server_scripts/villager_trades.js
+++ kubejs/server_scripts/villager_trades.js
@@ -1,10 +1,10 @@
 //priority: 0
 
 MoreJSEvents.villagerTrades((event) => {
-    event.removeVanillaTrades(['ae2:fluix_researcher'], 1)
-    event.removeVanillaTrades(['ae2:fluix_researcher'], 2)
-    event.removeVanillaTrades(['ae2:fluix_researcher'], 3)
-    event.removeVanillaTrades(['ae2:fluix_researcher'], 4)
-    event.removeVanillaTrades(['ae2:fluix_researcher'], 5)
+    // event.removeVanillaTrades(['ae2:fluix_researcher'], 1)
+    // event.removeVanillaTrades(['ae2:fluix_researcher'], 2)
+    // event.removeVanillaTrades(['ae2:fluix_researcher'], 3)
+    // event.removeVanillaTrades(['ae2:fluix_researcher'], 4)
+    // event.removeVanillaTrades(['ae2:fluix_researcher'], 5)
     //event.removeVanillaTrades(['toolsmith'],1)
 })
