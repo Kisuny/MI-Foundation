bc8d346a9f4d5ce6235f661c4da93d0c0b3d367d
Kisuny <homeca19@gmail.com>
2024-12-08 11:22:35 +0300

    removed group items for chipped

--- kubejs/client_scripts/REI/rei_groups.js
+++ kubejs/client_scripts/REI/rei_groups.js
@@ -1,38 +1,5 @@
-function replaceAll(string, find, replace) {
-    return string.replace(new RegExp(find, 'g'), replace)
-}
-function getNameOfID(id) {
-    let output = "";
-    id = replaceAll(id, ":", " ");
-    id = replaceAll(id, "_", " ");
-
-    let parts = id.split(" ");
-    for (let i = 0; i < parts.length; i++) {
-        output += parts[i].substring(0, 1).toUpperCase() + parts[i].substring(1);
-        if (i < parts.length - 1) {
-            output += " ";
-        } else {
-            output += "s";
-        }
-    }
-    return output;
-}
-
 REIEvents.groupEntries(event => {
-    Ingredient.of('@minecraft').getItemIds().forEach((id) => {
-        let name = getNameOfID(id);
-        id = id.replace('minecraft:', 'chipped:');
-        let parts = id.split(':');
-        event.groupItemsByTag(`kubejs:rei_groups/${parts[0]}/${parts[1]}`, name, id);
-    });
-
-    let terracottas = ['minecraft:terracotta'];
-    for(let i=1; i<=66; i++){
-        terracottas.push('chipped:terracotta_'+i);
-    }
-
     // Group items
-    event.groupItems('kubejs:reigroups/chipped/terracotta', getNameOfID('minecraft:terracotta'), terracottas);
     event.groupItems('kubejs:rei_groups/drills/gold', 'Gold Drills', [/.*gold_drill/]);
     event.groupItems('kubejs:rei_groups/drills/aluminum', 'Aluminum Drills', [/.*aluminum_drill/]);
     event.groupItems('kubejs:rei_groups/drills/bronze', 'Bronze Drills', [/.*bronze_drill/]);
@@ -52,17 +19,6 @@ REIEvents.groupEntries(event => {
     // УБРАТЬ
     event.groupItems('kubejs:rei_groups/MCW_windows', 'MCW Windows', [/mcwwindows:.*_window($|2)/]);
 
-    // Same items
-    event.groupSameItem("kubejs:rei_groups/common_barrels", "Common barrels", Item.of("sophisticatedstorage:barrel"));
-    event.groupSameItem("kubejs:rei_groups/common_barrels_1", "Common barrels T1", Item.of("sophisticatedstorage:limited_barrel_1"));
-    event.groupSameItem("kubejs:rei_groups/common_barrels_2", "Common barrels T2", Item.of("sophisticatedstorage:limited_barrel_2"));
-    event.groupSameItem("kubejs:rei_groups/common_barrels_3", "Common barrels T3", Item.of("sophisticatedstorage:limited_barrel_3"));
-    event.groupSameItem("kubejs:rei_groups/common_barrels_4", "Common barrels T4", Item.of("sophisticatedstorage:limited_barrel_4"));
-    event.groupSameItem("kubejs:rei_groups/common_chest", "Common chests", Item.of("sophisticatedstorage:chest"));
-    event.groupSameItem("kubejs:rei_groups/common_shulker_boxes", "Common shulker boxes", Item.of("sophisticatedstorage:shulker_box"));
-
-    event.groupSameItem("kubejs:rei_groups/yttr_mercurial_potions", "Yttr mercurial potions", Item.of("yttr:mercurial_potion"));
-    event.groupSameItem("kubejs:rei_groups/yttr_splash_mercurial_potions", "Yttr mercurial splash potions", Item.of("yttr:mercurial_splash_potion"));
     event.groupSameItem('kubejs:rei_groups/botania_brew_vial', 'Botania Brew Vials', Item.of("botania:brew_vial"));
     event.groupSameItem('kubejs:rei_groups/botania_brew_flask', 'Botania Brew Flasks', Item.of("botania:brew_flask"));
     event.groupSameItem('kubejs:rei_groups/botania_blood_pendant', 'Botania Blood Pendants', Item.of("botania:blood_pendant"));
