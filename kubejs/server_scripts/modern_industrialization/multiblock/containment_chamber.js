3523d16d4fcd0a90bf67b921198ecde52f53fcac
error1number404 <error1number404@gmail.com>
2025-01-25 14:59:13 +0300

    containment_chamber rework 1 itteration

--- kubejs/server_scripts/modern_industrialization/multiblock/containment_chamber.js
+++ kubejs/server_scripts/modern_industrialization/multiblock/containment_chamber.js
@@ -1,19 +1,19 @@
 //priority: 0
 
 ServerEvents.recipes(event => {
-    event.recipes.modern_industrialization.containment_chamber(256, 200) //Eu, ticks
-        .fluidIn('spectrum:liquid_crystal', 500)
+    event.recipes.modern_industrialization.containment_chamber(128, 200) //Eu, ticks
+        .fluidIn('spectrum:liquid_crystal', 100)
         .itemIn('8x #minecraft:leaves')
         .itemOut('spectrum:vegetal')
         .biome('ad_astra:orbit')
-    event.recipes.modern_industrialization.containment_chamber(512, 600) //Eu, ticks
-        .fluidIn('spectrum:midnight_solution', 500)
+    event.recipes.modern_industrialization.containment_chamber(128, 300) //Eu, ticks
+        .fluidIn('spectrum:midnight_solution', 100)
         .itemIn('minecraft:obsidian')
         .itemOut('spectrum:neolith')
         .biome('ad_astra:orbit')
 
-    event.recipes.modern_industrialization.containment_chamber(512, 600) //Eu, ticks
-        .fluidIn('spectrum:midnight_solution', 500)
+    event.recipes.modern_industrialization.containment_chamber(128, 300) //Eu, ticks
+        .fluidIn('spectrum:midnight_solution', 100)
         .itemIn('#c:raw_meat')
         .itemOut('4x spectrum:midnight_chip')
         .biome('ad_astra:orbit')
