b8d927be8d3d5b0bc7e558d237ec764c6ca4935f
Sipepper <valera1gribnik@gmail.com>
2025-02-15 01:09:45 +0200

    Orechid rebalance
    Rebalanced weights, the most important that modded ores are more common
    now

--- kubejs/server_scripts/botania/orechid.js
+++ kubejs/server_scripts/botania/orechid.js
@@ -1,8 +1,8 @@
 //priority: 0
 
 const overworld = [
-    { name: 'modern_industrialization:gadolinite_ore', weight: 20000 },
-    { name: 'modern_industrialization:brookite_ore', weight: 20000 },
+    { name: 'modern_industrialization:gadolinite_ore', weight: 2000 },
+    { name: 'modern_industrialization:brookite_ore', weight: 2000 },
     { name: 'modern_industrialization:antimony_ore', weight: 1000 },
     { name: 'modern_industrialization:bauxite_ore', weight: 1000 },
     { name: 'modern_industrialization:salt_ore', weight: 1000 },
@@ -12,8 +12,8 @@ const overworld = [
 ];
 
 const overworld_deepslate = [
-    { name: 'modern_industrialization:deepslate_gadolinite_ore', weight: 10000 },
-    { name: 'modern_industrialization:deepslate_brookite_ore', weight: 10000 },
+    { name: 'modern_industrialization:deepslate_gadolinite_ore', weight: 1000 },
+    { name: 'modern_industrialization:deepslate_brookite_ore', weight: 1000 },
     { name: 'modern_industrialization:deepslate_antimony_ore', weight: 500 },
     { name: 'modern_industrialization:deepslate_bauxite_ore', weight: 500 },
     { name: 'modern_industrialization:deepslate_salt_ore', weight: 500 },
@@ -22,59 +22,59 @@ const overworld_deepslate = [
 ];
 
 const nether = [
-    { name: 'spectrum:stratine_ore', weight: 400 },
+    { name: 'spectrum:stratine_ore', weight: 800 },
 ];
 
 const moon_planet = [
-    { name: 'ad_astra:moon_iron_ore', weight: 8000 },
-    { name: 'ad_astra:moon_desh_ore', weight: 4000 },
-    { name: 'ad_astra:moon_ice_shard_ore', weight: 2000 },
-    { name: 'ad_astra:moon_cheese_ore', weight: 500 },
+    { name: 'ad_astra:moon_iron_ore', weight: 500 },
+    { name: 'ad_astra:moon_desh_ore', weight: 400 },
+    { name: 'ad_astra:moon_ice_shard_ore', weight: 200 },
+    { name: 'ad_astra:moon_cheese_ore', weight: 50 },
 ];
 
 const mars_planet = [
-    { name: 'ad_astra:mars_iron_ore', weight: 8000 },
-    { name: 'ad_astra:mars_ice_shard_ore', weight: 3000 },
-    { name: 'ad_astra:mars_ostrum_ore', weight: 2000 },
-    { name: 'ad_astra:mars_diamond_ore', weight: 500 },
+    { name: 'ad_astra:mars_iron_ore', weight: 400 },
+    { name: 'ad_astra:mars_ice_shard_ore', weight: 300 },
+    { name: 'ad_astra:mars_ostrum_ore', weight: 200 },
+    { name: 'ad_astra:mars_diamond_ore', weight: 50 },
 ];
 
 const venus_planet = [
-    { name: 'ad_astra:venus_coal_ore', weight: 8000 },
-    { name: 'ad_astra:venus_gold_ore', weight: 2000 },
-    { name: 'ad_astra:venus_diamond_ore', weight: 1000 },
-    { name: 'ad_astra:venus_calorite_ore', weight: 500 },
+    { name: 'ad_astra:venus_coal_ore', weight: 400 },
+    { name: 'ad_astra:venus_gold_ore', weight: 200 },
+    { name: 'ad_astra:venus_diamond_ore', weight: 100 },
+    { name: 'ad_astra:venus_calorite_ore', weight: 50 },
 ];
 
 const mercury_planet = [
-    { name: 'ad_astra:mercury_iron_ore', weight: 8000 },
+    { name: 'ad_astra:mercury_iron_ore', weight: 800 },
 ];
 
 const glacio_planet = [
-    { name: 'ad_astra:glacio_iron_ore', weight: 8000 },
-    { name: 'ad_astra:glacio_coal_ore', weight: 6000 },
-    { name: 'ad_astra:glacio_copper_ore', weight: 4000 },
-    { name: 'ad_astra:glacio_ice_shard_ore', weight: 5000 },
-    { name: 'ad_astra:glacio_lapis_ore', weight: 1000 },
+    { name: 'ad_astra:glacio_iron_ore', weight: 800 },
+    { name: 'ad_astra:glacio_coal_ore', weight: 600 },
+    { name: 'ad_astra:glacio_copper_ore', weight: 400 },
+    { name: 'ad_astra:glacio_ice_shard_ore', weight: 500 },
+    { name: 'ad_astra:glacio_lapis_ore', weight: 100 },
 ];
 
 const deeper_down = [
-    { name: 'spectrum:blackslag_emerald_ore', weight: 1239 },
-    { name: 'spectrum:blackslag_coal_ore', weight: 67415 },
-    { name: 'spectrum:blackslag_redstone_ore', weight: 7654 },
-    { name: 'spectrum:blackslag_gold_ore', weight: 2647 },
-    { name: 'spectrum:blackslag_iron_ore', weight: 29371 },
-    { name: 'spectrum:blackslag_lapis_ore', weight: 1079 },
-    { name: 'spectrum:blackslag_diamond_ore', weight: 883 },
-    { name: 'spectrum:blackslag_copper_ore', weight: 7000 },
-    { name: 'spectrum:blackslag_topaz_ore', weight: 1000 },
-    { name: 'spectrum:blackslag_citrine_ore', weight: 1000 },
-    { name: 'spectrum:blackslag_amethyst_ore', weight: 1000 },
-    { name: 'spectrum:blackslag_onyx_ore', weight: 400 },
-    { name: 'spectrum:blackslag_moonstone_ore', weight: 100 },
-    { name: 'spectrum:blackslag_shimmerstone_ore', weight: 1200 },
-    { name: 'spectrum:blackslag_azurite_ore', weight: 800 },
-    { name: 'spectrum:blackslag_malachite_ore', weight: 50 },
+    { name: 'spectrum:blackslag_emerald_ore', weight: 100 },
+    { name: 'spectrum:blackslag_coal_ore', weight: 400 },
+    { name: 'spectrum:blackslag_redstone_ore', weight: 500 },
+    { name: 'spectrum:blackslag_gold_ore', weight: 250 },
+    { name: 'spectrum:blackslag_iron_ore', weight: 500 },
+    { name: 'spectrum:blackslag_lapis_ore', weight: 400 },
+    { name: 'spectrum:blackslag_diamond_ore', weight: 200 },
+    { name: 'spectrum:blackslag_copper_ore', weight: 300 },
+    { name: 'spectrum:blackslag_topaz_ore', weight: 200 },
+    { name: 'spectrum:blackslag_citrine_ore', weight: 200 },
+    { name: 'spectrum:blackslag_amethyst_ore', weight: 200 },
+    { name: 'spectrum:blackslag_onyx_ore', weight: 100 },
+    { name: 'spectrum:blackslag_moonstone_ore', weight: 50 },
+    { name: 'spectrum:blackslag_shimmerstone_ore', weight: 600 },
+    { name: 'spectrum:blackslag_azurite_ore', weight: 500 },
+    { name: 'spectrum:blackslag_malachite_ore', weight: 100 },
 ];
 
 const normal = [
