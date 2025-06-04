cd2c2cbbcb88639516ba4c9f609abf7b08c2130e
Kisuny <homeca19@gmail.com>
2025-06-04 15:51:17 +0300

    Changed the effects of opening the bag

--- kubejs/server_scripts/bags/lootbag.js
+++ kubejs/server_scripts/bags/lootbag.js
@@ -11,8 +11,8 @@ ItemEvents.rightClicked(event => {
             let dimension = event.player.getLevel().getDimension()
 
             event.server.runCommandSilent(`execute in ${event.level.dimension} run loot spawn ${x} ${y} ${z} loot ${lootable}`)
-            event.server.runCommandSilent(`playsound minecraft:entity.experience_orb.pickup player ${player} ${x} ${y} ${z} 0.5 0.75 0.1`)
-            event.server.runCommandSilent(`/execute in ${dimension} run particle minecraft:enchant ${x} ${y} ${z} 1 1 1 1 100 normal`)
+            event.server.runCommandSilent(`playsound spectrum:shatter_light player ${player} ${x} ${y} ${z} 0.5 2 0.1`)
+            event.server.runCommandSilent(`/execute in ${dimension} run particle adventurez:amethyst_shard_particle ${x} ${y} ${z} 1 1 1 1 100 normal`)
 
             // In creative don't use bag
             if (!event.player.isCreative()) { event.item.count-- }
