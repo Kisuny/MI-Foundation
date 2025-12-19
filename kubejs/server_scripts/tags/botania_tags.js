efe4b4a44741f352c255280e3c976a89ae20f4c6
Роман Коваль <homeca19@gmail.com>
2025-12-19 06:14:36 +0300

    Botania loonium now can drop bronze coins and runic_tablet

--- kubejs/server_scripts/tags/botania_tags.js
+++ kubejs/server_scripts/tags/botania_tags.js
@@ -11,6 +11,6 @@ ServerEvents.tags('item', event => {
         event.add('botania:apothecary', `botania:apothecary_${apothecary.type}`)
     });
 
-    event.add('botania:loonium_blacklist', ['numismatic-overhaul:gold_coin', 'numismatic-overhaul:silver_coin', 'numismatic-overhaul:bronze_coin', 'kubejs:reforge_rune', 'simplyswords:runic_tablet', 'artifacts:steadfast_spikes', 'neepmeat:integrator_egg', 'artifacts:universal_attractor', 'majruszsaccessories:adventurer_kit', 'artifacts:umbrella', 'artifacts:plastic_drinking_hat', 'artifacts:novelty_drinking_hat', 'artifacts:scarf_of_invisibility', 'artifacts:bunny_hoppers', 'things:rabbit_foot_charm', 'things:socks', 'majruszsaccessories:removal_card', 'artifacts:steadfast_spikes', 'artifacts:helium_flamingo', '#mif:exploring_trinkets', '#simplyswords:uniques'])
+    event.add('botania:loonium_blacklist', ['numismatic-overhaul:gold_coin', 'numismatic-overhaul:silver_coin', 'kubejs:reforge_rune', 'artifacts:steadfast_spikes', 'neepmeat:integrator_egg', 'artifacts:universal_attractor', 'majruszsaccessories:adventurer_kit', 'artifacts:umbrella', 'artifacts:plastic_drinking_hat', 'artifacts:novelty_drinking_hat', 'artifacts:scarf_of_invisibility', 'artifacts:bunny_hoppers', 'things:rabbit_foot_charm', 'things:socks', 'majruszsaccessories:removal_card', 'artifacts:steadfast_spikes', 'artifacts:helium_flamingo', '#mif:exploring_trinkets', '#simplyswords:uniques'])
 
 });
