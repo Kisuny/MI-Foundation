aeaee4bd226d89382f5d8e242f7e8518ee207693
Роман Коваль <homeca19@gmail.com>
2025-12-19 05:38:31 +0300

    Added bewitchment shelfs in banned list for modpack (server crash)

--- kubejs/server_scripts/server_stuff/banned_items.js
+++ kubejs/server_scripts/server_stuff/banned_items.js
@@ -1,4 +1,20 @@
-let bannedItems = [['spectrum:perturbed_eye', 'Doesnt work in modpack'], ['kibe:cooler', 'Food duping']];
+let bannedItems = [
+  ['spectrum:perturbed_eye', 'Doesnt work in modpack'], 
+  ['kibe:cooler', 'Food duping'],
+  ['bewitchment:oak_poppet_shelf', 'Server crash'],
+  ['bewitchment:spruce_poppet_shelf', 'Server crash'],
+  ['bewitchment:birch_poppet_shelf', 'Server crash'],
+  ['bewitchment:jungle_poppet_shelf', 'Server crash'],
+  ['bewitchment:acacia_poppet_shelf', 'Server crash'],
+  ['bewitchment:dark_oak_poppet_shelf', 'Server crash'],
+  ['bewitchment:crimson_poppet_shelf', 'Server crash'],
+  ['bewitchment:warped_poppet_shelf', 'Server crash'],
+  ['bewitchment:juniper_poppet_shelf', 'Server crash'],
+  ['bewitchment:cypress_poppet_shelf', 'Server crash'],
+  ['bewitchment:elder_poppet_shelf', 'Server crash'],
+  ['bewitchment:dragons_blood_poppet_shelf', 'Server crash'],
+];
+
 bannedItems.forEach((pair) => {
   PlayerEvents.inventoryChanged(pair[0], event => {
     event.player.tell([
